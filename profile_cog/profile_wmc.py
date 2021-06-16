35642983731d4d31ff0c23b2090aa4da02cd69d5
Tejas Agarwal <67542663+tejasag@users.noreply.github.com>
2021-06-16 13:20:04 +0530

    Update profile_wmc.py

--- profile_cog/profile_wmc.py
+++ profile_cog/profile_wmc.py
@@ -1,6 +1,6 @@
 import discord
 from redbot.core import commands, Config
-from typing import Optional
+from typing import Optionalw
 
 
 class ProfileCogWmc(commands.Cog):
@@ -36,14 +36,19 @@ class ProfileCogWmc(commands.Cog):
             if not v:
                 continue
             if k == "screenshot_link" and v is not None:
-                embed.set_image(url=v)
+                
+                    embed.set_image(url=v)
+                else:
+                    
                 continue
             embed.add_field(name=f"**{k}**", value=v, inline=True)
         embed.set_author(name=member.name, icon_url=member.avatar_url_as())
         await ctx.reply(embed=embed)
 
     @profile.command(name="set")
-    async def set(self, ctx, field, *, value):
+    async def set(self, ctx, field=None, *, value=None):
+        if not field:
+            return await ctx.reply("Choose one from: `shell`, `distro`, `bar`, `system_specs`, `wm_de`, `dotfiles`, `terminal`, `screenshot_link`, `editor`, `theme`\nCommand Usage: `-profile set [field] [value]`")
         field = field.lower()
         fields = [
             "shell",
@@ -69,6 +74,7 @@ class ProfileCogWmc(commands.Cog):
             return await ctx.send(
                 f"Bruh, you cannot set that ://\n Choose one from: `shell`, `distro`, `bar`, `system_specs`, `wm_de`, `dotfiles`, `terminal`, `screenshot_link`, `editor`, `theme`"
             )
+        
 
         field = "system_specs" if field in ["system_specs", "systemspecs"] else field
         field = (
@@ -77,9 +83,19 @@ class ProfileCogWmc(commands.Cog):
             else field
         )
         field = "screenshot_link" if field in ["screenshot_link", "screenshot", "image"] else field
+        if field == "screenshot_link":
+            if not (value.startswith("https://") and  (value.endswith("png") or value.endswith("jpg") or value.endswith("jpeg") or value.endswith("gif") or value.endswith("webp"))):
+               return await ctx.send("Please enter a valid image/gif url for profile screenshot!")
         old = await self.config.user(ctx.author).profile()
+
+        if not value:
+            old[field] = None
+            await self.config.user(ctx.author).profile.set(old)
+            return ctx.reply(f"Done! I have reset the `{field} field!`")
+
         old[field] = value
         await self.config.user(ctx.author).profile.set(old)
+
         embed = discord.Embed(
             title=f"Done!",
             description=f"The value of `{field}` is now `{value}`",
